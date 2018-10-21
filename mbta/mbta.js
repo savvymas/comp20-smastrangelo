037098090ee5fd5c8056b913bde17290e90d34ff
Savannah Mastrangelo <savannah.mastrangelo@tufts.edu>
2018-10-21 15:42:43 -0400

    marker test

--- mbta/mbta.js
+++ mbta/mbta.js
@@ -5,6 +5,6 @@ function initMap() {
     center: southStation,
     zoom: 8
   });
-  var trainStation = "https://www.google.com/search?q=train+station+emoji&rlz=1C5CHFA_enUS772US772&source=lnms&tbm=isch&sa=X&ved=0ahUKEwj4lM69o5jeAhXylOAKHR-wDmwQ_AUIDigB&biw=1214&bih=755#imgrc=t9GcRR2izbj6jM:";
-  var marker = new google.maps.Marker({position: southStation, map: map, icon: trainStation});
+  var image = 'https://www.google.com/search?q=train+station+emoji&rlz=1C5CHFA_enUS772US772&source=lnms&tbm=isch&sa=X&ved=0ahUKEwj4lM69o5jeAhXylOAKHR-wDmwQ_AUIDigB&biw=1214&bih=755#imgrc=t9GcRR2izbj6jM:';
+  var marker = new google.maps.Marker({position: southStation, map: map, icon: image});
 }
