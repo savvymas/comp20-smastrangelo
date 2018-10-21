8210bf82957e8720e63c569915435f331167f412
Savannah Mastrangelo <savannah.mastrangelo@tufts.edu>
2018-10-21 16:06:48 -0400

    add all station stops polylines

--- mbta/mbta.js
+++ mbta/mbta.js
@@ -8,11 +8,53 @@ function initMap() {
   var image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
   var marker = new google.maps.Marker({position: southStation, map: map, icon: image});
 
+  var Andrew = {lat:  42.330154, lng: -71.057655};
+  var porterSq = {lat: 42.3884, lng: -71.11914899999999};
+  var harvardSq = {lat: 42.373362, lng:  -71.118956};
+  var jkfUmass = {lat: 42.320685, lng: -71.052391};
+  var savinHill = {lat: 42.31129, lng: -71.053331};
+  var parkStreet = {lat: 42.35639457, lng: -71.0624242};
+  var broadway = {lat: 42.342622, lng: -71.056967};
+  var northQuincy = {lat: 42.275275, lng: -71.029583};
+  var shawmut = {lat:42.29312583, lng: -71.06573796000001};
+  var davis = {lat: 42.39674, lng: -71.121815};
+  var alewife = {lat: 42.395428, lng: -71.142483};
+  var kendallMIT = {lat: 42.36249079, lng: -71.08617653};
+  var charlesMGH = {lat: 42.361166, lng:  -71.070628};
+  var downtownCrossing = {lat:  42.355518, lng: -71.060225};
+  var quincyCenter = {lat:  42.251809, lng: -71.005409};
+  var quincyAdams = {lat:  42.233391, lng: -71.007153};
+  var ashmont = {lat: 42.284652, lng: -71.06448899999999};
+  var wollaston = {lat: 42.2665139, lng: -71.0203369};
+  var fieldsCorner = {lat: 42.300093, lng: -71.061667};
+  var centralSquare = {lat:  42.365486, lng:  -71.103802};
+  var braintree = {lat: 42.2078543, lng:  -71.0011385};
+
   var stationCoordinates = [
-    {lat: 42.352271, lng: -71.05524200000001},
-    {lat:  42.330154, lng: -71.057655},
-    {lat: 42.3884, lng: -71.11914899999999},
-    {lat: 42.373362, lng:  -71.118956}
+    southStation,
+    Andrew,
+    porterSq,
+    harvardSq,
+    jkfUmass,
+    savinHill,
+    parkStreet,
+    broadway,
+    northQuincy,
+    shawmut,
+    davis, 
+    alewife, 
+    kendallMIT,
+    charlesMGH,
+    downtownCrossing, 
+    quincyCenter,
+    quincyAdams,
+    ashmont,
+    wollaston,
+    fieldsCenter,
+    fieldsCorner,
+    centralSquare,
+    braintree
+
   ];
   var flightPath = new google.maps.Polyline({
     path: stationCoordinates,
