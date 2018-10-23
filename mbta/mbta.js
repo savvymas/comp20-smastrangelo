b9982a45cdfdfe9c2670b7ba002ef8f1d5fe5a52
Savannah Mastrangelo <savannah.mastrangelo@tufts.edu>
2018-10-23 15:25:20 -0400

    test

--- mbta/mbta.js
+++ mbta/mbta.js
@@ -118,6 +118,18 @@ function initMap() {
   var myLat = 0;
   var myLng = 0;
   var me = new google.maps.LatLng(myLat, myLng);
+  var myOptions = {
+    zoom: 13, // The larger the zoom number, the bigger the zoom
+    center: me,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+   };
+  var map;
+  var marker;
+  var infowindow = new google.maps.InfoWindow();
+
+    map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+    getMyLocation();
+    
 
     function renderMap() {
         console.log("in render map");
