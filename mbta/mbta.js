c5b2dc352b59030e61629a51f9e4c1e4ad20072e
Savannah Mastrangelo <savannah.mastrangelo@tufts.edu>
2018-10-23 13:53:05 -0400

    bug test

--- mbta/mbta.js
+++ mbta/mbta.js
@@ -113,38 +113,37 @@ function initMap() {
   ashmontPath.setMap(map);
 
 
-    
-
-}
-
-function renderMap() {
-    me = new google.maps.LatLng(myLat, myLng);
-    // Update map and go there...
-    map.panTo(me);
-    
-    // Create a marker
-    marker = new google.maps.Marker({
-        position: me,
-        title: "Here I Am!"
-    });
-    marker.setMap(map);
+    function renderMap() {
+        me = new google.maps.LatLng(myLat, myLng);
+        // Update map and go there...
+        map.panTo(me);
         
-    // Open info window on click of marker
-    google.maps.event.addListener(marker, 'click', function() {
-        infowindow.setContent(marker.title);
-        infowindow.open(map, marker);
-    });
-} 
-
-function getMyLocation() {
-    if (navigator.geolocation) { // the navigator.geolocation object is supported on your browser
-        navigator.geolocation.getCurrentPosition(function(position) {
-            myLat = position.coords.latitude;
-            myLng = position.coords.longitude;
-            renderMap();
+        // Create a marker
+        marker = new google.maps.Marker({
+            position: me,
+            title: "Here I Am!"
         });
+        marker.setMap(map);
+            
+        // Open info window on click of marker
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.setContent(marker.title);
+            infowindow.open(map, marker);
+        });
+    } 
+
+    function getMyLocation() {
+        if (navigator.geolocation) { // the navigator.geolocation object is supported on your browser
+            navigator.geolocation.getCurrentPosition(function(position) {
+                myLat = position.coords.latitude;
+                myLng = position.coords.longitude;
+                renderMap();
+            });
+        }
+        else {
+            alert("Geolocation is not supported by your web browser.  What a shame!");
+        }
     }
-    else {
-        alert("Geolocation is not supported by your web browser.  What a shame!");
-    }
+    
+
 }
