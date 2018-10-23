6c2fccfb399bc1c7d3ae04d59c759767ec7a04f7
Savannah Mastrangelo <savannah.mastrangelo@tufts.edu>
2018-10-22 21:42:42 -0400

    set var map

--- mbta/mbta.js
+++ mbta/mbta.js
@@ -1,7 +1,7 @@
 var map;
 var southStation =  {lat: 42.352271, lng: -71.05524200000001};
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
+  var map = new google.maps.Map(document.getElementById('map'), {
     center: southStation,
     zoom: 8
   });
