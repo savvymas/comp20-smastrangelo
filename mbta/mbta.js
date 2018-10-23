91fe0b5ed2d5a23c8a940e79965d0acad55d1051
Savannah Mastrangelo <savannah.mastrangelo@tufts.edu>
2018-10-23 16:31:49 -0400

    create marker

--- mbta/mbta.js
+++ mbta/mbta.js
@@ -27,9 +27,7 @@ function initMap() {
 
     function handleLocationError(browserHasGeolocation, infoWindow, pos) {
         infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                                'Error: The Geolocation service failed.' :
-                                'Error: Your browser doesn\'t support geolocation.');
+        myMarker = new google.maps.Marker({position: pos, map: map});
         infoWindow.open(map);
     }
 
