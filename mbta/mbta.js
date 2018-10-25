c9fc11160a362e36ea9585ede92ade7053a71480
Savannah Mastrangelo <savannah.mastrangelo@tufts.edu>
2018-10-25 13:18:25 -0400

    test

--- mbta/mbta.js
+++ mbta/mbta.js
@@ -189,7 +189,7 @@ function findShortestDistance(pos) {
 
     var allDistances = [];
 
-    allStops.forEach(calcDistances(item)) {
+    allStops.forEach(calcDistances(item)){
         var dis = google.maps.geometry.spherical.computeDistanceBetween(pos, southStation);
         allDistances = allDistances.push(dis);
     };
